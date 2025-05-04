3b08ccab6f2e2c3790fe3702ac63b35afaf57356
clyde tan <clydeenriquetan@gmail.com>
2025-05-04 15:22:07 +0800

    fixed vite config base name

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/company-website-quotation"
+  base: "/company-website-quotation/"
 });
